9ab05711ef25f159c8b5b7c9aa6e321b89c9d089
Rico Steiner <rico@Ricos-MacBook-Pro-2.local>
2019-12-26 16:00:35 +0100

    Override permissions

--- index.php
+++ index.php
@@ -1,5 +1,42 @@
 <?php
 
-Kirby::plugin('gsteirico/kirby-plugin-panel-acl', [
-    // plugin magic happens here
+@include_once __DIR__ . '/vendor/autoload.php';
+
+Kirby::plugin('steirico/kirby-plugin-panel-acl', [
+    'blueprints' => [
+        'users/my-role' => [
+            'title'    => 'MyRole',
+            'name'    => 'MyRole',
+            'description'    => 'MyRole',
+            'permissions' => [
+                'access' => [
+                    'users' => false,
+                    'site' => true,
+                    'settings' => false,
+                    'panel' => true
+                ],
+                'site' => [
+                    'changeTitle' => false,
+                    'update'      => false
+                ],
+                'pages' => [
+                    'changeSlug'     => function(){ 
+                        return true;
+                    },
+                    'changeStatus'   => false,
+                    'changeTemplate' => false,
+                    'changeTitle'    => false,
+                    'create'         => false,
+                    'delete'         => false,
+                    'duplicate'      => false,
+                    'preview'        => false,
+                    'read'           => function($page, $user){
+                        return true;
+                    },
+                    'sort'           => false,
+                    'update'         => false
+                ],
+            ]
+        ]
+    ]
 ]);
